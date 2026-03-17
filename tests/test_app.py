3fc5ffdb9536230776fef12a0b7950d17bd43057
James McMilan <B00391814@studentmail.uw.ac.uk>
2026-03-17 10:43:22 +0000

    add failing test for pounds to kg bug

--- tests/test_app.py
+++ tests/test_app.py
@@ -34,3 +34,11 @@ def test_grams_to_pounds():
     # if this test fails, the function grams_to_pounds is incorrect
     assert pytest.approx(grams_to_pounds(1000), rel=1e-5) == 2.20462
 
+from app import main_conversion_function
+
+import pytest
+def test_main_conversion_pounds_to_kg():
+    result = main_conversion_function(10, 'pounds', 'kg')
+    assert pytest.approx(result, rel=1e-5) == 4.53592
+
+
